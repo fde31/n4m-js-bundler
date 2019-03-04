eba806216cccb98a1fc9f9077846bd0f9a6ceddb
Florian Demmer <florian@cycling74.com>
2019-03-04 15:05:07 +0000

    extended boilerplate to showcase that fe ES6 classes and import of core node modules works

--- n4m.js-bundler.js
+++ n4m.js-bundler.js
@@ -1,21 +1,37 @@
+import { EventEmitter } from "events";
 
-const log = (...args) => {
-	post(`${args.join(", ")}\n`);
-};
+// Showcasing that you can use an ES6 Class here
+class Logger extends EventEmitter {
+
+	constructor() {
+		super();
+	}
+
+	log(...args) {
+		post(`${args.join(", ")}\n`);
+		this.emit("log", ...args);
+	}
+}
+
+const logger = new Logger();
+
+// We are using the EventEmitter class to showcase access to Node modules
+logger.on("log", (...args) => {
+	post("---\n");
+});
 
 const bang = number => {
-	log("Received bang");
+	logger.log("Received bang");
 };
 
 const msg_int = number => {
-	log(`Received int: ${number}`);
+	logger.log(`Received int: ${number}`);
 };
 
 const msg_float = number => {
-	log(`Received float: ${number}`);
+	logger.log(`Received float: ${number}`);
 };
 
 const anything = (...args) => {
-	log(`Received anything:`);
-	log(args);
+	logger.log(`Received anything: ${args}`);
 };
